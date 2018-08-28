c81a7ff31cff3e43f4e700d203e00447707c7966
Huego <hugozaanen@hotmail.com>
2018-08-28 15:58:44 +0200

    test

---
+++ flags/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace flags
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.answerButton = new System.Windows.Forms.Button();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // answerButton
+            // 
+            this.answerButton.Location = new System.Drawing.Point(36, 220);
+            this.answerButton.Name = "answerButton";
+            this.answerButton.Size = new System.Drawing.Size(75, 23);
+            this.answerButton.TabIndex = 1;
+            this.answerButton.Text = "Answer";
+            this.answerButton.UseVisualStyleBackColor = true;
+            this.answerButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(36, 272);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(100, 20);
+            this.inputBox.TabIndex = 2;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(36, 63);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(172, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(543, 416);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 310);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Secret Answer ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 346);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 390);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(887, 564);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.answerButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button answerButton;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ flags/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace flags
+{
+    //test bericht 2
+    public partial class Form1 : Form
+    {
+        string[] files = Directory.GetFiles(@"flags/", "*.png");
+        Dictionary<string, string> nameCode = new Dictionary<string, string>();
+        Dictionary<string, string> countryCodes = new Dictionary<string, string>();
+        Random ranNum = new Random();
+        string answer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            using (var reader = new StreamReader(@"flag_information.csv"))
+            {
+                
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    var code = values[1];
+                    var fullCountry = values[0];
+                    countryCodes.Add(code, fullCountry);
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(inputBox.Text == label1.Text)
+            {
+                label1.Text = "Yeah! :D";
+                label1.Visible = true;
+                label2.Text = answer;
+                inputBox.Clear();
+            }
+            else
+            {
+                label1.Visible = true;
+                label1.Text = "Naww :(";
+                label2.Text = answer;
+                inputBox.Clear();
+                
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            int num = ranNum.Next(0,files.Length);
+            string result = Path.GetFileNameWithoutExtension(files[num]);
+            pictureBox1.Image = new Bitmap(files[num]);
+            countryCodes.TryGetValue(result.ToUpper(), out answer);
+            //inputBox.Text = result;
+            label1.Visible = false;
+            label1.Text = answer;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int num = ranNum.Next(0, files.Length);
+            string result = Path.GetFileNameWithoutExtension(files[num]);
+            pictureBox1.Image = new Bitmap(files[num]);
+            countryCodes.TryGetValue(result.ToUpper(), out answer);
+            label1.Visible = false;
+            label1.Text = answer;
+        }
+    }
+}
